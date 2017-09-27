3902a88dc22dbe92cf7610fdf8c05a47a04aa75f
narowi <naroaoyarzabal@hotmail.com>
2017-09-27 10:06:32 +0200

    prueba2 karpeta ezabatu eta hauteskundeetan mahai berri bat gehitu nire izenarekin

--- hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
+++ hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
@@ -24,7 +24,8 @@ public class Hauteskundeak {
 		h2012.gehituMahaia("Irun");
 		h2012.gehituMahaia("Donostia");
 		h2012.gehituMahaia("Tolosa");
-		
+		h2012.gehituMahaia("Bilbo");
+
 		System.out.println(h2012);
 	}
 
